cecaaf3e7dc210be10347968e658adef28569bd6
Rodolfo-Rodrigues <50324906+Rodolfo-Rodrigues@users.noreply.github.com>
2019-06-17 12:18:22 -0300

    Update menuResponsivo.js

--- menuResponsivo.js
+++ menuResponsivo.js
@@ -1,11 +1,28 @@
+/* código usando jquery
+$(document).ready(function () {
+    $('.show-menu').click(function () {
+        $('.menu').toggleClass('active')
+    })
+})
+*/
+
 let menu = document.querySelector('.menu');
 let showMenu = document.querySelector('.show-menu');
-let hideMenu = document.querySelector('.hide-menu');
+/*let hideMenu = document.querySelector('.hide-menu'); */
 
 showMenu.addEventListener('click', function () {
-	menu.classList.add('active');
+    menu.classList.toggle('active');
+    showMenu.classList.toggle('ativo');
+})
+
+/*
+código com js puro usando o show e hide menu e classList.add e classList.remove
+
+showMenu.addEventListener('click', function () {
+    menu.classList.add('active');
 })
 
 hideMenu.addEventListener('click', function () {
 	menu.classList.remove('active');
 })
+*/
